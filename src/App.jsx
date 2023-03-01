9cf2ce6bdcb0102b5e3b0aeafc4d76ab63f924f2
Jair Romero Peña <jair@equipo3.com>
2023-03-01 16:27:23 -0600

    Seperacion

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,32 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div>
+   <div align = "center" className="mt-3">
     <h3>Contador</h3>
     <hr />
-    <h1>{count}</h1>
+    <h1 className="mb-3">{count}</h1>
     
-    <button 
+<button 
      onClick={() => {
       setCount(count + 1);
     }} 
      type="button" 
-     className="btn btn-primary"
+     className="btn btn-primary m-1"
      >+1</button>
-
-  <button 
+<button 
      onClick={() => {
       setCount(count - 1);
     }} 
      type="button" 
-     className="btn btn-primary"
+     className="btn btn-danger m-1"
      >-1</button>
-
 <button 
      onClick={() => {
       setCount(0);
     }} 
      type="button" 
-     className="btn btn-primary"
+     className="btn btn-info m-1"
      > 0</button>
-
 
    </div>
   )
